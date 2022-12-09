72c8a364be9a0c89c2971d6cb8eac79bf827f608
daniil.sudzhaev <git@sudzhaev.com>
2022-12-10 00:48:31 +0200

    day 9 – optimized and cleaned up

--- src/main/kotlin/Day9.kt
+++ src/main/kotlin/Day9.kt
@@ -1,4 +1,4 @@
-import java.io.InputStreamReader
+import kotlin.math.absoluteValue
 import kotlin.math.sign
 
 
@@ -10,51 +10,49 @@ val Coordinate.y
     get() = second
 
 fun day9(length: Int) {
-    val tailPositionSet = mutableSetOf<Coordinate>()
     val offsetMap = mapOf(
-        "R" to (1 to 0),
-        "L" to (-1 to 0),
-        "U" to (0 to 1),
-        "D" to (0 to -1)
+        'R' to (1 to 0),
+        'L' to (-1 to 0),
+        'U' to (0 to 1),
+        'D' to (0 to -1)
     )
+    val noOffset = 0 to 0
 
     fun move(knot: Coordinate, offset: Coordinate): Coordinate {
+        if (offset == noOffset) {
+            return knot
+        }
         return knot.x + offset.x to knot.y + offset.y
     }
 
     fun getOffset(knot1: Coordinate, knot2: Coordinate): Coordinate {
-        val touching = knot1.toList().zip(knot2.toList())
-            .all { pair -> pair.x - pair.y in setOf(0, 1, -1) }
+        // touching is a boolean that indicates whether the two knots are touching both horizontally, vertically and diagonally
+        val touching = (knot1.x - knot2.x).absoluteValue <= 1 && (knot1.y - knot2.y).absoluteValue <= 1
         if (touching) {
-            return 0 to 0
+            return noOffset
         }
         return (knot1.x - knot2.x).sign to (knot1.y - knot2.y).sign
     }
 
-    fun moveRope(rope: MutableList<Pair<Int, Int>>) {
-        for (i in rope.indices) {
-            if (i == 0) {
-                continue
-            }
-
+    fun moveRope(rope: MutableList<Coordinate>, headOffset: Coordinate) {
+        rope[0] = move(rope.first(), headOffset)
+        for (i in rope.indices.drop(1)) {
             val offset = getOffset(rope[i - 1], rope[i])
             rope[i] = move(rope[i], offset)
-
-            if (i == rope.size - 1) {
-                tailPositionSet += rope[i]
-            }
         }
     }
 
-    val rope = (0 until length).map { 0 to 0 }.toMutableList()
+    val rope = MutableList(length) { 0 to 0 }
+    val tailPositionSet = mutableSetOf<Coordinate>()
 
-    InputStreamReader(System.`in`).forEachLine { line ->
-        val split = line.split(" ")
-        val direction = split[0]
+    readStdin { line ->
+        val split = line.split(' ')
+        val direction = split[0].first()
         val distance = split[1].toInt()
+        val offset = offsetMap[direction] ?: error("invalid direction: $direction")
         for (i in 0 until distance) {
-            rope[0] = move(rope.first(), offsetMap[direction]!!)
-            moveRope(rope)
+            moveRope(rope, offset)
+            tailPositionSet += rope.last()
         }
     }
 
