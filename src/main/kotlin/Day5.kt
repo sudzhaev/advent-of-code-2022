20c96748ce9f6abcafea1a823e6d596be9fac8a8
daniil.sudzhaev <git@sudzhaev.com>
2022-12-13 00:14:18 +0200

    day 5 – refactoring

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -2,69 +2,76 @@ import java.io.InputStreamReader
 import java.util.*
 
 // https://adventofcode.com/2022/day/5
-// that's a mess, I was doing it at 2AM
-// I'll rewrite it later
-
-fun day5part1() {
-    fun parse(line: String): List<String> {
-        return line.chunked(4).map { it.trim(' ', ']', '[') }
-    }
-
-    fun parseInitialStateMap(rawInitialStateMap: List<List<String>>): List<Stack<String>> {
-        val initialStateMap = mutableListOf<Stack<String>>()
-        for (i in rawInitialStateMap[0].indices) {
-            initialStateMap.add(Stack())
+// part 1 – false
+// part 2 – true
+fun day5(takeAllAtOnce: Boolean) {
+    fun parseState(lines: List<String>): List<Stack<Char>> {
+        fun split(line: String): List<Char?> {
+            return line.chunked(4) // '[A] ' or '[A]'
+                .map { it.trim(' ', ']', '[') }
+                .map {
+                    when (it.length) {
+                        0 -> null
+                        1 -> it[0]
+                        else -> error("invalid input: $it in line $line")
+                    }
+                }
         }
-        for (strings in rawInitialStateMap.reversed()) {
-            for ((i, string) in strings.withIndex()) {
-                if (string.isNotEmpty()) {
-                    initialStateMap[i].push(string)
+
+        fun MutableList<Stack<Char>>.getOrDefault(index: Int): Stack<Char> {
+            val value = getOrNull(index)
+            return value ?: Stack<Char>().also { add(it) }
+        }
+
+        val initialStateMap = arrayListOf<Stack<Char>>()
+        for (line in lines.take(lines.size).reversed()) {
+            for ((i, char) in split(line).withIndex()) {
+                if (char != null) {
+                    initialStateMap.getOrDefault(i).push(char)
                 }
             }
         }
         return initialStateMap
     }
 
-    val lines = InputStreamReader(System.`in`).readLines()
-    val rawInitialStateMap = mutableListOf<List<String>>()
-    var stopIdx = 0;
-    for ((i, line) in lines.withIndex()) {
-        if (line.isEmpty()) {
-            stopIdx = i
-            break
-        }
-        if (!line.contains('[')) {
-            continue
-        }
-        rawInitialStateMap += parse(line)
-    }
-
-    val initialStateMap = parseInitialStateMap(rawInitialStateMap)
-    println(initialStateMap)
-
-    for (line in lines.drop(stopIdx + 1)) {
+    fun parseCommand(
+        line: String,
+        initialStateMap: List<Stack<Char>>
+    ): Triple<Int, Stack<Char>, Stack<Char>> {
         val split = line.split(" ")
         val n = split[1].toInt()
         val source = split[3].toInt()
         val sourceStack = initialStateMap[source - 1]
         val target = split[5].toInt()
         val targetStack = initialStateMap[target - 1]
+        return Triple(n, sourceStack, targetStack)
+    }
 
-        val itemsToMove = mutableListOf<String>()
-        for (i in 0 until n) {
-            // part 1 move
-            // targetStack.push(sourceStack.pop())
-            itemsToMove += sourceStack.pop()
-        }
+    val lines = InputStreamReader(System.`in`).readLines()
+    val splitIndex = lines.indexOf("")
+    check(splitIndex != -1) { "empty line not found" }
 
-        for (s in itemsToMove.reversed()) {
-            targetStack.push(s)
+    val state = parseState(lines.take(splitIndex - 1))
+
+    for (line in lines.drop(splitIndex + 1)) {
+        val (n, sourceStack, targetStack) = parseCommand(line, state)
+
+        if (takeAllAtOnce) {
+            // part 2
+            val buffer = arrayListOf<Char>()
+            for (i in 1..n) {
+                buffer += sourceStack.pop()
+            }
+            for (char in buffer.reversed()) {
+                targetStack.push(char)
+            }
+        } else {
+            // part 1
+            for (i in 1..n) {
+                targetStack.push(sourceStack.pop())
+            }
         }
     }
 
-    println(initialStateMap.map { it.peek() }.joinToString(""))
-}
-
-fun main() {
-    day5part1()
+    println(state.map { it.peek() }.joinToString(""))
 }
