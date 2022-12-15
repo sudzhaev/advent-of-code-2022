1dfad682722b8ebada3892de7358d05f7647535c
daniil.sudzhaev <git@sudzhaev.com>
2022-12-15 21:41:35 +0200

    day 15 – part 2

--- src/main/kotlin/Day15.kt
+++ src/main/kotlin/Day15.kt
@@ -2,6 +2,13 @@ import kotlin.math.abs
 
 const val MANHATTAN_PI = 4
 
+data class Circle(val center: Coordinate, val radius: Int) {
+
+    fun contains(coordinate: Coordinate): Boolean {
+        return abs(center.x - coordinate.x) + abs(center.y - coordinate.y) <= radius
+    }
+}
+
 data class Sensor(val x: Int, val y: Int) {
 
     fun distanceTo(beacon: Beacon): Int {
@@ -11,7 +18,7 @@ data class Sensor(val x: Int, val y: Int) {
     fun circle(radius: Int): Set<Coordinate> {
         val circle = LinkedHashSet<Coordinate>(2 * MANHATTAN_PI * radius)
         var yDeviation = 0
-        for (currentX in x - radius .. x + radius) {
+        for (currentX in x - radius..x + radius) {
             circle += currentX to y + yDeviation
             circle += currentX to y - yDeviation
             if (currentX >= x) {
@@ -42,11 +49,13 @@ fun day15() {
 
     val xValues = mutableSetOf<Int>()
     val xBeaconValues = mutableSetOf<Int>()
+    val circles = mutableListOf<Circle>()
 
     readStdin { line ->
         val (sensor, beacon) = parse(line)
         val distance = sensor.distanceTo(beacon)
         val circle = sensor.circle(distance)
+        circles += Circle(sensor.x to sensor.y, distance)
         val yCirclePoints = circle.filter { it.y == targetY }
         if (yCirclePoints.isNotEmpty()) {
             when (yCirclePoints.size) {
@@ -58,6 +67,7 @@ fun day15() {
                         xValues += x
                     }
                 }
+
                 else -> error("there cannot be more than 2 points")
             }
         }
@@ -66,7 +76,26 @@ fun day15() {
         }
     }
 
-    println((xValues - xBeaconValues).count())
+    println((xValues - xBeaconValues).size)
+
+    var targetBeacon: Coordinate? = null
+    loop@
+    for (x in 0..4_000_000) {
+        var y = 0
+        while (y <= 4_000_000) {
+            val coordinate = x to y
+            val circle = circles.firstOrNull { it.contains(coordinate) }
+            if (circle == null) {
+                targetBeacon = coordinate
+                break@loop
+            }
+
+            val xDistance = circle.center.x - x
+            y = circle.center.y + circle.radius - xDistance + 1
+        }
+    }
+    check(targetBeacon != null) { "target beacon not found" }
+    println(targetBeacon)
 }
 
 fun main() {
